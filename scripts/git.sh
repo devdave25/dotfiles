9ea39e5ff39a2f897ad3938655f2a2ee4b5d11e4
David Alexander <me@dja.dev>
2023-06-06 11:24:03 +0100

    chore(git): add .gitignore_global file

--- scripts/git.sh
+++ scripts/git.sh
@@ -2,8 +2,31 @@ git config --global user.name "David Alexander"
 git config --global user.email "me@dja.dev"
 git config --global user.username "devdave25"
 
+git config --global core.excludesfile ~/.gitignore_global
 
 git config --global --add alias.lola 'log --graph --decorate --abbrev-commit --all --date=local --pretty=format:"%C(auto)%h%d %C(blue)%an %C(green)%cd %C(red)%GG %C(reset)%s" --date=local'
 git config --global --add alias.lol 'log --graph --decorate --abbrev-commit --all --oneline'
 
 git config --global rebase.autosquash true
+
+dotpath=".."
+
+copyfile() {
+  if [ ! -f $1 ]; then
+    echo "Could not find \"$1\""
+  else
+    echo "Copying \"$1\" to \"$2\""
+    cp $1 $2
+  fi
+}
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    copyfile "${dotpath}/git/.gitignore_global" "${HOME}/.gitignore_global"
+elif [[ "$OSTYPE" == "msys"* ]]; then
+    # Windows
+    copydir "${dotpath}/git/.gitignore_global" "${HOME}/.gitignore_global"
+else
+    # Unknown.
+    echo "Unknown OS - Missing config file copy commands."
+fi
