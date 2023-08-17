56946bd1bc4916432273fe52d280104aad0a99f0
David Alexander <me@dja.dev>
2023-08-17 22:33:19 +0100

    chore(nvim): change nerdtree to nvim-tree

--- nvim/lua/plugins/nerdtree.lua
+++
@@ -1,9 +0,0 @@
--- NerdTree
-
-return {
-  "preservim/nerdtree",
-  config = function()
-    vim.api.nvim_set_keymap('n', '<leader>no', ':NERDTreeToggle<CR>', { noremap = true })
-    vim.api.nvim_set_keymap('n', '<leader>nf', ':NERDTreeFind<CR>', { noremap = true })
-  end
-}
