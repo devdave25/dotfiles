2157063413ddc25e2b7d17d865f28cf36240a97c
David Alexander <me@dja.dev>
2024-05-07 12:40:05 +0100

    chore(lsp): uppdate lsp configuration

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
 return {
   'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
+  branch = 'v3.x',
   dependencies = {
     -- LSP Support
     { 'neovim/nvim-lspconfig' },             -- Required
@@ -8,16 +8,19 @@ return {
     { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
     -- Autocompletion
-    { 'hrsh7th/nvim-cmp' },         -- Required
-    { 'hrsh7th/cmp-nvim-lsp' },     -- Required
-    { 'hrsh7th/cmp-buffer' },       -- Optional
-    { 'hrsh7th/cmp-path' },         -- Optional
-    { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-    { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+    { 'hrsh7th/nvim-cmp' },     -- Required
+    { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+    { 'hrsh7th/cmp-buffer' },   -- Optional
+    { 'hrsh7th/cmp-path' },     -- Optional
+    { 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
     -- Snippets
     { 'L3MON4D3/LuaSnip' },             -- Required
+    { 'saadparwaiz1/cmp_luasnip' },     -- Optional
     { 'rafamadriz/friendly-snippets' }, -- Optional
+
+    -- UI
+    { 'j-hui/fidget.nvim' },
   },
   config = function()
     local lsp = require('lsp-zero').preset({})
@@ -26,32 +29,95 @@ return {
     lsp.on_attach(function(_, bufnr)
       local opts = { buffer = bufnr }
 
-      vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-      vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-      vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-      vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-      vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-      vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-      vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-      vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-      vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-      vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+      vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+      vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+      vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
+      vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
+      vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, opts)
+      vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
+      vim.keymap.set('n', 'gs', function() vim.lsp.buf.signature_help() end, opts)
+      vim.keymap.set('n', '<F2>', function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set({ 'n', 'x' }, '<F3>', function() vim.lsp.buf.format({ async = true }) end, opts)
+      vim.keymap.set('n', '<F4>', function() vim.lsp.buf.code_action() end, opts)
 
-      vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-      vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
-      vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+      vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end, opts)
+      vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
+      vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts)
     end)
 
-    lsp.ensure_installed({
-      -- Replace these with whatever servers you want to install
-      'tsserver',
-      'eslint',
-      'tailwindcss'
+    require('fidget').setup({})
+    require('mason').setup({})
+    require('mason-lspconfig').setup({
+      ensure_installed = {
+        'tsserver',
+        'eslint',
+        'tailwindcss'
+      },
+      handlers = {
+        lsp.default_setup,
+        -- function(server_name)
+        --   require('lspconfig')[server_name].setup({})
+        -- end,
+
+        -- this is the "custom handler" for `lua_ls`
+        lua_ls = function()
+          local lua_opts = lsp.nvim_lua_ls()
+          require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+      },
     })
 
-    -- (Optional) Configure lua language server for neovim
-    lsp.nvim_workspace()
+    local cmp = require('cmp')
+    local cmp_format = lsp.cmp_format()
+
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+    -- this is the function that loads the extra snippets to luasnip
+    -- from rafamadriz/friendly-snippets
+    require('luasnip.loaders.from_vscode').lazy_load()
+
+    cmp.setup({
+      formatting = cmp_format,
+      sources = {
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip', keyword_length = 2 },
+        { name = 'buffer',  keyword_length = 3 },
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+      }),
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+    })
+
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' }
+      }
+    })
 
     lsp.setup()
+
+    vim.diagnostic.config({
+      -- update_in_insert = true,
+      float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    })
   end,
 }
